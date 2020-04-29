61136eafae92ede3c5a61576a3ccbc11149ed2f4
Luca Beltrami <>
2020-04-29 15:13:37 -0700

    Added unit tests per the group roll parsing

--- src/dice/StandardDice.js
+++ src/dice/StandardDice.js
@@ -73,7 +73,7 @@ class StandardDice {
     } else if (typeof value === 'object') {
       modifiers = new Map(Object.entries(value));
     } else {
-      throw new TypeError('modifiers should be a Map or an Object');
+      throw new TypeError('modifiers should be a Map, an Array, or an Object');
     }
 
     if (
